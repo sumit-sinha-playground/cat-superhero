326e1ec971be230276f72a51b6e1981c6498473c
Sumit Sinha <sumitsinha0615@gmail.com>
2026-01-31 18:18:03 +0100

    Ad sound on cat movements

--- scripts/walking_cat_character_body_2d.gd
+++ scripts/walking_cat_character_body_2d.gd
@@ -6,8 +6,11 @@ extends CharacterBody2D
 # Get the gravity from the project settings
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Updated to match your screenshot name exactly
+# Node References (Matched to your .tscn unique names)
 @onready var anim = $walking_cat_animation_2D
+@onready var sfx_idle = $SfxIdle
+@onready var sfx_jump = $SfxJump
+@onready var sfx_run = $SfxRun
 
 func _physics_process(delta):
 	# 1. Apply Gravity
@@ -15,31 +18,51 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# 2. Handle Jump (Space Bar)
-	# "ui_accept" is mapped to Space by default in Godot
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 		anim.play("jump")
+		sfx_jump.play() # Play jump once
 
-	# 3. Get Movement Input (Left/Right Arrows)
+	# 3. Get Movement Input (Left/Right)
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
 		velocity.x = direction * speed
 		anim.flip_h = direction < 0
 		
-		# Only play walk if we are touching the ground
 		if is_on_floor():
 			anim.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-		
-		# Only play idle if we are touching the ground
 		if is_on_floor():
 			anim.play("idle")
 
 	# 4. Air Animation logic
-	# If we are falling and not currently playing the jump animation
 	if not is_on_floor() and velocity.y > 0:
-		anim.play("jump") # Or a "fall" animation if you have one
+		anim.play("jump")
 
+	# 5. Apply movement
 	move_and_slide()
+	
+	# 6. Handle Sound States
+	manage_audio(direction)
+
+func manage_audio(direction: float):
+	if is_on_floor():
+		if direction != 0:
+			if not sfx_run.playing:
+				sfx_run.play()
+
+			if sfx_idle.playing:
+				sfx_idle.stop()
+		else:
+			if not sfx_idle.playing:
+				sfx_idle.play()
+
+			if sfx_run.playing:
+				sfx_run.stop()
+	else:
+		if sfx_run.playing:
+			sfx_run.stop()
+		if sfx_idle.playing:
+			sfx_idle.stop()
