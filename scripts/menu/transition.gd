21fe2f1de22f5c8646c0f77c649c2441971e6e13
David Broder-Rodgers <broder93@gmail.com>
2026-01-31 21:21:06 +0100

    Add sound effect on transition

--- scripts/menu/transition.gd
+++ scripts/menu/transition.gd
@@ -1,10 +1,15 @@
 extends TextureRect
 
+@onready var _animation_player = $AnimationPlayer
+@onready var _transition_audio = $TransitionAudio
+
 func _ready() -> void:
-	$AnimationPlayer.play_backwards("spin")
+	_animation_player.play_backwards("spin")
+	await _animation_player.animation_finished
+	_transition_audio.stop()
 
 func transition_to(next_scene) -> void:
-	var anim_player = $AnimationPlayer
-	anim_player.play("spin")
-	await anim_player.animation_finished
+	_animation_player.play("spin")
+	_transition_audio.play()
+	await _animation_player.animation_finished
 	get_tree().change_scene_to_file(next_scene)
