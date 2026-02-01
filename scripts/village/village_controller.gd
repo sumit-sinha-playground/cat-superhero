436a66710ee84e131e29a2c347b195a493bd2c10
Sumit Sinha <sumitsinha0615@gmail.com>
2026-02-01 14:55:33 +0100

    Fix the cat walking logic

--- scripts/village/village_controller.gd
+++ scripts/village/village_controller.gd
@@ -4,17 +4,3 @@ extends Node2D
 @export var walk_direction: int = 1
 
 @onready var cat = get_node_or_null(cat_path)
-
-func _process(delta: float) -> void:
-	if not cat:
-		return
-	var spd = 400
-	if "speed" in cat:
-		spd = cat.speed
- 
-	cat.velocity.x = spd * walk_direction
-	
-	var anim = cat.get_node_or_null("walking_cat_animation_2D")
-	if anim and cat.is_on_floor():
-		anim.play("walk")
-		anim.flip_h = walk_direction < 0
